e0d1a8f311502a88ce73244b63cc4f42687075cf
tanvir005 <tanvir3550gmail.com>
2022-04-19 19:50:24 +0600

    Checkout page and final  Update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div className="m-40">
-            <div className="p-10">
-                <h1 className="text-3xl text-slate-700 font-bold text-left">Difference between authorization and authentication.</h1>
-                <p className="ml-5 text-2xl text-slate-500 text-left"> Authentication is a procedure,
+        <div className="md:m-40 m-10">
+            <div className="md:p-10 p-5">
+                <h1 className="md:text-3xl text-slate-700 font-bold text-left">Difference between authorization and authentication.</h1>
+                <p className="md:ml-5 md:text-2xl text-slate-500 text-left"> Authentication is a procedure,
                     in which the identification of users is checked for giving the access to the system.
                     Whereas authorization is the procedure of verifying what particular applications,
                     files, and data a user has to access. Authentication is executed earlier than the
@@ -13,9 +13,9 @@ const Blogs = () => {
                     while the authorization procedure is executed after the authentication procedure.
                 </p>
             </div>
-            <div className="p-10">
-                <h1 className="text-3xl text-slate-700 font-bold text-left">Why are you using firebase? What other options do you have to implement authentication?</h1>
-                <p className="ml-5 text-2xl text-slate-500 text-left">
+            <div className="md:p-10 p-5">
+                <h1 className="md:text-3xl text-slate-700 font-bold text-left">Why are you using firebase? What other options do you have to implement authentication?</h1>
+                <p className="md:ml-5 md:text-2xl text-slate-500 text-left">
                     We use FIrebase to make our authentication easy.
                     Because Firebase Authentication provides ready-made UI libraries, easy-to-use SDKs,
                     and backend services to authenticate users to our Web app. We can authenticate using
@@ -23,14 +23,14 @@ const Blogs = () => {
                     Facebook and Twitter, and more.
 
                 </p>
-                <h1 className="text-2xl text-slate-700 font-bold text-left">Best Open Source Firebase Alternatives:</h1>
-                <ul className="pl-5">
-                    <li>AWS Amplify</li>
-                    <li> Flutter</li>
-                    <li>LoopBack</li>
-                    <li> SashiDo</li>
-                    <li>Parse</li>
-                    <li>Back4App</li>
+                <h1 className="md:text-2xl text-slate-700 font-bold text-left">Best Open Source Firebase Alternatives:</h1>
+                <ul className="md:pl-5 md:ml-5 md:text-2xl text-slate-500 text-left">
+                    <li >AWS Amplify</li>
+                    <li > Flutter</li>
+                    <li >LoopBack</li>
+                    <li > SashiDo</li>
+                    <li >Parse</li>
+                    <li >Back4App</li>
                 </ul>
             </div>
 
--- src/components/VerificationSend/VerificationSend.js
+++ src/components/VerificationSend/VerificationSend.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const VerificationSend = () => {
     return (
         <div>
-            <h1 className="text-slate-800 font-semibold text-4xl text-center p-36">An varification link send to your email. <br /> <span className="text-green-900">Please check your email and active your account.</span> </h1>
-            <h1 className="text-slate-800 font-semibold text-4xl text-center pb-44">You can read our <Link className="text-emerald-500 hover:underline hover:text-emerald-600" to="/blogs"> blogs.</Link>  </h1>
+            <h1 className="text-slate-800 font-semibold md:text-4xl text-center md:p-36 p-10">An varification link send to your email. <br /> <span className="text-green-900">Please check your email and active your account.</span> </h1>
+            <h1 className="text-slate-800 font-semibold md:text-4xl text-center md:pb-44 p-16">You can read our <Link className="text-emerald-500 hover:underline hover:text-emerald-600" to="/blogs"> blogs.</Link>  </h1>
 
         </div>
     );
